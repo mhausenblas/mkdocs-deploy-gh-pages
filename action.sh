05e0a01a61d6ad8e00aaf54d84957cf48ab7f1c4
Michael Hausenblas <hausenbl@amazon.com>
2019-12-25 08:07:48 +0000

    fixes action script

--- action.sh
+++ action.sh
@@ -3,8 +3,5 @@
 set -e
 set -o pipefail
 
-echo 'Installing MkDocs'
-pip install mkdocs
-
 echo 'Building the MkDocs site'
 ./mkdocs gh-deploy --config-file "${GITHUB_WORKSPACE}/${INPUT_SITE_DIR}/mkdocs.yml"
