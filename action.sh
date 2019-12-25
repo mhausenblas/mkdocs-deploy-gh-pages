8a3f6194676f334c282daed15a8124015875857c
Michael Hausenblas <hausenbl@amazon.com>
2019-12-25 07:22:10 +0000

    fixes action

--- action.sh
+++ action.sh
@@ -3,20 +3,6 @@
 set -e
 set -o pipefail
 
-if [[ -n "$TOKEN" ]]; then
-	GITHUB_TOKEN=$TOKEN
-fi
-
-if [[ -z "$GITHUB_TOKEN" ]]; then
-	echo "Need GITHUB_TOKEN environment variable to proceed"
-	exit 1
-fi
-
-if [[ -z "$TARGET_REPO" ]]; then
-	echo "Need TARGET_REPO environment variable to proceed"
-	exit 1
-fi
-
 if [[ -z "$SITE_DIR" ]]; then
 	SITE_DIR=docs
 fi
