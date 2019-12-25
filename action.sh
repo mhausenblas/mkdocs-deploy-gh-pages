a953d8305f3a6f5d0f1d2ec4e3187b718840796e
Michael Hausenblas <hausenbl@amazon.com>
2019-12-25 12:09:06 +0000

    fixes script

--- action.sh
+++ action.sh
@@ -2,4 +2,13 @@
 
 set -e
 
+
+if ! git config --get user.name; then
+    git config --global user.name "${GITHUB_ACTOR}"
+fi
+
+if ! git config --get user.email; then
+    git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+fi
+
 mkdocs gh-deploy --config-file "${GITHUB_WORKSPACE}/mkdocs.yml"
