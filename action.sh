f9c104358bfd236308d74e02157abcc29a6196e2
Michael Hausenblas <hausenbl@amazon.com>
2019-12-25 11:36:55 +0000

    fixes script

--- action.sh
+++ action.sh
@@ -2,8 +2,4 @@
 
 set -e
 
-if [[ -z "${SITE_DIR}" ]]; then
-	SITE_DIR=docs/
-fi
-
-mkdocs gh-deploy --config-file "${GITHUB_WORKSPACE}/${SITE_DIR}mkdocs.yml"
+mkdocs gh-deploy --config-file "${GITHUB_WORKSPACE}/mkdocs.yml"
