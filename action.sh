0f35f4056bcf9abc08609cd095f3acea14827bc9
Michael Hausenblas <hausenbl@amazon.com>
2019-12-25 11:18:23 +0000

    fixes script

--- action.sh
+++ action.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
-./mkdocs gh-deploy --config-file "${GITHUB_WORKSPACE}/${INPUT_SITE_DIR}/mkdocs.yml"
+mkdocs gh-deploy --config-file "${GITHUB_WORKSPACE}/${INPUT_SITE_DIR}/mkdocs.yml"
