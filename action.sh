55cb662aded131e2761eebec7b6e48b3044375db
Kenneth East <khe@east.fm>
2022-10-14 17:51:46 -0500

    add debugging output

--- action.sh
+++ action.sh
@@ -61,8 +61,8 @@ fi
 git remote rm origin
 git remote add origin "${remote_repo}"
 
-print_info("pwd: $(pwd)")
-print_info("ls -ltr /github: $(ls -ltr /github)")
-print_info("ls -ltr: $(ls -ltr)")
+print_info "pwd: $(pwd)"
+print_info "ls -ltr /github: $(ls -ltr /github)"
+print_info "ls -ltr: $(ls -ltr)"
 
 mkdocs gh-deploy --config-file "${CONFIG_FILE}" --force
